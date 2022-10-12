d3f6e3de13fe6c20184874122274295b9ba8359b
haawwkeye <connorbishop53@gmail.com>
2022-10-12 15:29:55 -0400

    escapeHtml func, temp rankings, current user Info

--- Web/JS/admin.js
+++ Web/JS/admin.js
@@ -1,3 +1,33 @@
+//TODO: Add a sendHttp function that can be used with await and return data without a callback
+//      This is very usefull as rn we can only do everything once the request is done instead of waiting
+//      for the request to be done before doing anything else
+
+const rankings = {
+    [-1]: "Player",
+    [0]: "VIP",
+    [1]: "Trial Mod",
+    [2]: "Mod",
+    [3]: "Admin",
+    [4]: "Super Admin",
+    [5]: "Developer",
+    [6]: "Owner"
+}
+
+/**
+ * 
+ * @param {String} unsafe 
+ * @returns {String} escaped
+ */
+const escapeHtml = (unsafe) => {
+    if (typeof(unsafe) != "string") return "";
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
 /**
  * 
  * @param {String} type
@@ -24,7 +54,33 @@ function sendhttp(type, url, body, callback)
     xhr.send(body);
 }
 
-sendhttp("get", "/api/isAdmin", null, (err, res) => {
-    if (err) console.error(err);
-    console.log(res);
-})
+// We want to wait for everything to load before attempting to do any of this since this script is probably at the top of body
+document.addEventListener("DOMContentLoaded", () => {
+    try {
+        let userInfo;
+        sendhttp("get", "/api/userInfo", null, (err, res) => {
+            if (err)
+            {
+                console.error(err);
+                throw err;
+            }
+            userInfo = JSON.parse(res);
+
+            let user = {
+                Username: escapeHtml(userInfo.Username),
+                PermissionLevel: userInfo.PermissionLevel,
+                IsAdmin: userInfo.IsAdmin
+            }
+
+            let infoElem = document.getElementById("UserInfo");
+            let userHtml = document.createElement("b");
+            userHtml.className = "User";
+            userHtml.innerHTML = `${user.IsAdmin && "[SITE ADMIN]" || ""}[${rankings[user.PermissionLevel].toUpperCase() || user.PermissionLevel}] ${user.Username}`;
+    
+            infoElem.appendChild(userHtml);
+        });
+    } catch (error) {
+        console.error(error);
+        alert(error);
+    }
+});
