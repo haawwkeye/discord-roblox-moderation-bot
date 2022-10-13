a9e4de948447a534f1312c760fe959044fbb6949
haawwkeye <connorbishop53@gmail.com>
2022-10-12 23:11:28 -0400

    case sensitivity fix for mysql (just learnt of this issue)

--- Web/AdminPanel.sql
+++ Web/AdminPanel.sql
@@ -15,9 +15,9 @@
 
 CREATE TABLE `Users` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `username` varchar(50) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
   `isAdmin` tinyint(1) NOT NULL DEFAULT '0',
   `permissionLevel` int NOT NULL DEFAULT '-1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
