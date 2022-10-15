8a4c9b63aa729a7509c37731164ba83fae8f778a
haawwkeye <connorbishop53@gmail.com>
2022-10-15 18:28:44 -0400

    example command

--- example.command.js
+++ example.command.js
@@ -3,34 +3,41 @@
 //      to make sure everything works
 
 const Discord = require('discord.js');
-const ms = require('ms');
-
-require('dotenv').config();
-
-const embedColor = process.env.embedColor;
 
 // This is the roblox command this is what we will be looking for on the admin website
 // with this we can also only show commands that have this Roblox function
-exports.Roblox = async() => {
-
-}
-
-//TODO: Rewrite the run function as it only works with discord messages
-//      And we now only use interactions (make new function???)
-//      ofc message commands should probably still work but I'm lazy rn
-
-/**
-* @param {Discord.Message} message
-* @param {Discord.Client} client
-* @param {String[]} args
-*/
-
-exports.run = async(message, client, args) => {
-
-}
+exports.Roblox = async() => {}
 
 exports.help = async() => {
     let name = `**example**`;
     let description = "This command does nothing";
     return `${name} - ${description}\n`;
+}
+
+/**
+ * 
+ * @param {Discord.SlashCommandBuilder} builder 
+ * @returns {Discord.SlashCommandBuilder} built
+ */
+exports.build = (builder) => {
+    return builder
+            .setName('questionnaire')
+            .setDescription('Asks you a series of questions!')
+            .addStringOption(option => option.setName('input').setDescription('Your name?'))
+            .addBooleanOption(option => option.setName('bool').setDescription('True or False?'))
+            .addUserOption(option => option.setName('target').setDescription('Closest friend?'))
+            .addChannelOption(option => option.setName('destination').setDescription('Favourite channel?'))
+            .addRoleOption(option => option.setName('role').setDescription('Least favourite role?'))
+            .addIntegerOption(option => option.setName('int').setDescription('Sides to a square?'))
+            .addNumberOption(option => option.setName('num').setDescription('Value of Pi?'))
+            .addMentionableOption(option => option.setName('mentionable').setDescription('Mention something!'))
+            .addAttachmentOption(option => option.setName('attachment').setDescription('Best meme?'));
+}
+
+/**
+* @param {Discord.Interaction<CacheType>} interaction
+* @param {Discord.Client} client
+*/
+exports.run = async(interaction, client) => {
+    throw Error("Not Implemented");
 }
