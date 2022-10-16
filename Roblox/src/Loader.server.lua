e57f0421089d9923c8273278775a049c3ea47cbe
haawwkeye <connorbishop53@gmail.com>
2022-10-16 13:21:14 -0400

    wait to task.wait

--- Roblox/src/Loader.server.lua
+++ Roblox/src/Loader.server.lua
@@ -1,4 +1,6 @@
 --# selene: allow(global_usage, unused_variable, shadowing)
+local wait = task.wait;
+
 local HttpsService = game:GetService("HttpService")
 local adminSystem = require(script.Parent.MainModule)
 
@@ -16,24 +18,24 @@ end
 
 while true do
 	local request
-	
+
 	local suc, err = pcall(function()
 		request = HttpsService:GetAsync(_G.baseReplUrl .. _G.getRequestEndpoint)
 	end)
-	
+
 	if err then
 		warn("There was an error while attempting to connect to the administration webserver: " .. err)
 	end
-	
+
 	local newRequest
-	
+
 	local s, e = pcall(function()
 		newRequest = HttpsService:JSONDecode(request)
 	end)
-	
+
 	if s == true then
 		adminSystem:Run(newRequest.type, newRequest)
 	end
-	
+
 	wait(5)
 end
