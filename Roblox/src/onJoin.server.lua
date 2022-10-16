56288668a16d9b83e9785a3a0d54a5bc39a48de4
haawwkeye <connorbishop53@gmail.com>
2022-10-16 13:21:54 -0400

    move PlayerAdded into one function instead of 3, wait to task.wait

--- Roblox/src/onJoin.server.lua
+++ Roblox/src/onJoin.server.lua
@@ -1,74 +1,81 @@
 --# selene: allow(global_usage, unused_variable, shadowing)
+local wait = task.wait;
+
 local dataStoreService = game:GetService("DataStoreService")
 local playerService = game:GetService("Players")
+
+-- TODO: Use one DataStore for data instead of two?
+-- Example: DATA[uid] = {BanData, MuteData} etc
 
 local globalBanDataStore = dataStoreService:GetDataStore("bans")
 local globalMuteDataStore = dataStoreService:GetDataStore("mutes")
 
-playerService.PlayerAdded:Connect(function(plr)
-	local userID = tostring(plr.UserId)
-	local dataStoreKey = userID .. "-banData"
-	
-	local banData
-	
-	local s,e = pcall(function()
-		banData = globalBanDataStore:GetAsync(dataStoreKey)
-	end)
-	
-	if e then
-		plr:Kick("Couldn't load data, please rejoin")
-	end
-	
-	if banData == nil then
-		banData = {
-			isBanned = false
-		}
-	end
-	
-	if banData.isBanned == true then
-		local banMessage = "You are banned from this game\nModerator: " .. banData.moderator .. "\nReason: " .. banData.reason
-		plr:Kick(banMessage)
-	end
-end)
+function plrAdded(plr)
+	-- Server Locked
 
-playerService.PlayerAdded:Connect(function(plr)
 	local lockData = _G.lockData
-	
+
 	if lockData == nil then
 		lockData = {
 			isLocked = false
 		}
 	end
-	
+
 	if lockData.isLocked == true then
 		local lockMessage = "This server is locked!\nModerator: " .. lockData.moderator .. "\nReason: " .. lockData.reason
-		plr:Kick(lockMessage)
+		return plr:Kick(lockMessage);
 	end
-end)
 
-playerService.PlayerAdded:Connect(function(plr)
+	-- Ban Data
+
+	local userID = tostring(plr.UserId)
+	local dataStoreKey = userID .. "-banData"
+
+	local banData
+
+	local s,e = pcall(function()
+		banData = globalBanDataStore:GetAsync(dataStoreKey)
+	end)
+
+	if e then
+		return plr:Kick("Couldn't load data, please rejoin")
+	end
+
+	if banData == nil then
+		banData = {
+			isBanned = false
+		}
+	end
+
+	if banData.isBanned == true then
+		local banMessage = "You are banned from this game\nModerator: " .. banData.moderator .. "\nReason: " .. banData.reason
+		return plr:Kick(banMessage)
+	end
+
+	-- Mute Data
+
 	local userID = tostring(plr.UserId)
 	local dataStoreKey = userID .. "-muteData"
-	
+
 	local muteData
 
 	local s,e = pcall(function()
 		muteData = globalMuteDataStore:GetAsync(dataStoreKey)
 	end)
-	
+
 	if e then
-		plr:Kick("Couldn't load data, please rejoin")
+		return plr:Kick("Couldn't load data, please rejoin")
 	end
-	
+
 	if muteData == nil then
 		muteData = {
 			isMuted = false
 		}
 	end
-	
+
 	local chatService = require(game:GetService("ServerScriptService"):WaitForChild("ChatServiceRunner"):WaitForChild("ChatService"))
 	local channel = chatService:GetChannel("All")
-	
+
 	if muteData.isMuted == true then
 		local suc = false
 		repeat
@@ -76,11 +83,17 @@ playerService.PlayerAdded:Connect(function(plr)
 			local s,e = pcall(function()
 				channel:MuteSpeaker(plr.Name)
 			end)
-			
+
 			if s then
 				suc = true
 			end
 			wait()
 		until suc == true
 	end
-end)
+end
+
+for _, plr in pairs(game:GetService("Players"):GetPlayers()) do
+	plrAdded(plr);
+end
+
+playerService.PlayerAdded:Connect(plrAdded);
