f67f5e760b184e8d71d27ce021b03c402d0373a8
haawwkeye <connorbishop53@gmail.com>
2022-10-16 20:30:07 -0400

    Add todo for roblox

---
+++ Roblox/src/TODO.lua
@@ -0,0 +1,6 @@
+--[[
+    TODO: Revamp MainModule
+    TODO: Revamp GUI Instead of doing stuff on the server do it on the client
+          as well as moving the GUI to client on join and use remotes etc
+    TODO: Make a command system with the modules (maybe?)
+]]
