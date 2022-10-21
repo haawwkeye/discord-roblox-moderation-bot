8b546fb6bf1480831991733b377454e2e3b1804f
haawwkeye <connorbishop53@gmail.com>
2022-10-20 22:52:28 -0400

    temp add everything back for escapeHtml (waiting for better func)

--- Web/JS/admin.js
+++ Web/JS/admin.js
@@ -23,9 +23,9 @@ const escapeHtml = (unsafe) => {
     return unsafe
         .replace(/&/g, "&amp;")
         .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;");
-        // .replace(/"/g, "&quot;")
-        // .replace(/'/g, "&#039;");
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
 }
 
 /*
