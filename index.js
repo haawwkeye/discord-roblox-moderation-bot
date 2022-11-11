bf24f799933cbca14f84fc80062fc6e7bedc120b
haawwkeye <connorbishop53@gmail.com>
2022-11-11 12:12:58 -0500

    temp remove better-logging (doesn't work on windows?)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const args = process.argv.slice(2);
 
-require("better-logging")(console);
+// require("better-logging")(console);
 require('dotenv').config();
 
 const commandModule = require("./modules/commands");
